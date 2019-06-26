32ae453c3af46a102e0b79269ae56d1064ccfb93
Ananth <ks.ananth1987@gmail.com>
2019-06-26 18:57:33 +0200

    Added assets files
    Added assets files
    
    Co-Authored-By: Ananth K S <ksananth@users.noreply.github.com>

--- app/src/main/java/com/backbase/mobiletest/ui/citymap/presenter/CityListPresenter.java
+++ app/src/main/java/com/backbase/mobiletest/ui/citymap/presenter/CityListPresenter.java
@@ -4,16 +4,17 @@ import android.content.Context;
 
 import com.backbase.mobiletest.ui.citymap.contract.CityList;
 
+import java.lang.ref.WeakReference;
+
 public class CityListPresenter implements CityList.Presenter {
 
     private Context context;
-    private CityList.View view;
+    private WeakReference<CityList.View> view;
     private CityList.Model model;
 
     public CityListPresenter(Context context, CityList.View view, CityList.Model model) {
-
         this.context = context;
-        this.view = view;
+        this.view = new WeakReference<>(view);
         this.model = model;
     }
 }
