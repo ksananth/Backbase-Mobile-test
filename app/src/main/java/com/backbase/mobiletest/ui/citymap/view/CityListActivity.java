7cda018a22ef111568ea7f1b31944b9bd6e43dca
Ananth <anan.busyguy007@gmail.com>
2019-06-27 01:38:22 +0200

    optimize imports

--- app/src/main/java/com/backbase/mobiletest/ui/citymap/view/CityListActivity.java
+++ app/src/main/java/com/backbase/mobiletest/ui/citymap/view/CityListActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 import com.backbase.mobiletest.R;
 import com.backbase.mobiletest.data.AppDataManager;
