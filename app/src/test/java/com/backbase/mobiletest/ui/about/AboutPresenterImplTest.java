aecbdb26ba791587f9607a0af4d922a8b7a80bae
Ananth <anan.busyguy007@gmail.com>
2019-06-28 21:22:48 +0200

    added test shouldShowProgress_When_getAboutInfo

--- app/src/test/java/com/backbase/mobiletest/ui/about/AboutPresenterImplTest.java
+++ app/src/test/java/com/backbase/mobiletest/ui/about/AboutPresenterImplTest.java
@@ -9,8 +9,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static com.backbase.mobiletest.ui.citymap.model.CityListModel.CITIES_FILENAME;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,5 +33,12 @@ public class AboutPresenterImplTest {
     @Test
     public void shouldBeAbleToInitializePresenter() {
         assertNotNull(presenter);
+    }
+
+    @Test
+    public void shouldShowProgress_When_getAboutInfo() {
+        presenter.getAboutInfo();
+
+        Mockito.verify(view).showProgress();
     }
 }
