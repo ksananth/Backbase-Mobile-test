ea3c5d93375182bf425660a05acb7e04a4d4967f
Ananth <anan.busyguy007@gmail.com>
2019-06-28 21:17:03 +0200

    Added test for AboutPresenterImpl

---
+++ app/src/test/java/com/backbase/mobiletest/ui/about/AboutPresenterImplTest.java
@@ -0,0 +1,35 @@
+package com.backbase.mobiletest.ui.about;
+
+import android.content.Context;
+
+import com.backbase.mobiletest.ui.citymap.model.CityListModel;
+import com.backbase.mobiletest.ui.citymap.presenter.CityListPresenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AboutPresenterImplTest {
+
+    @Mock
+    About.View view;
+    @Mock
+    Context context;
+
+    private AboutPresenterImpl presenter;
+
+    @Before
+    public void setUp() {
+        presenter = new AboutPresenterImpl(view, context);
+    }
+
+    @Test
+    public void shouldBeAbleToInitializePresenter() {
+        assertNotNull(presenter);
+    }
+}
