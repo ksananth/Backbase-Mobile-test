21ad161d58a50afd2fae703a278be79fdeaec7bc
Ananth <anan.busyguy007@gmail.com>
2019-06-29 01:09:00 +0200

    added test shouldLoadMap_When_AnyCityClicked

--- app/src/androidTest/java/com/backbase/mobiletest/ui/citymap/view/CityListActivityTest.java
+++ app/src/androidTest/java/com/backbase/mobiletest/ui/citymap/view/CityListActivityTest.java
@@ -2,25 +2,23 @@ package com.backbase.mobiletest.ui.citymap.view;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
 
 import com.backbase.mobiletest.R;
 
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class CityListActivityTest {
@@ -38,7 +36,17 @@ public class CityListActivityTest {
     @Test
     public void shouldLoadCityList_When_OpenApp() throws InterruptedException {
         Thread.sleep(5000);
+
         onView(withId(R.id.frameLayout)).check(getViewAssertion());
+    }
+
+
+    @Test
+    public void shouldLoadMap_When_AnyCityClicked() throws InterruptedException  {
+        Thread.sleep(5000);
+
+        onView(withId(R.id.list_city)).perform(actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.map)).check(getViewAssertion());
     }
 
     private ViewAssertion getViewAssertion() {
