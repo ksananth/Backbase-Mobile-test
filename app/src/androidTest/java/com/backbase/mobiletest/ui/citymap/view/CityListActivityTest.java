a2ce922b03a3aef0b9c0d1fdc32c2bc0e2a04919
Ananth <anan.busyguy007@gmail.com>
2019-06-29 00:41:57 +0200

    Adding Android test dependencies and added test to check pacakge name

--- app/src/androidTest/java/com/backbase/mobiletest/ui/citymap/view/CityListActivityTest.java
+++ app/src/androidTest/java/com/backbase/mobiletest/ui/citymap/view/CityListActivityTest.java
@@ -1,21 +1,21 @@
-package com.backbase.mobiletest;
+package com.backbase.mobiletest.ui.citymap.view;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class CityListActivityTest {
+    @Rule
+    public ActivityTestRule<CityListActivity> rule = new ActivityTestRule<>(CityListActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
